23e62e0e6e81e1c85fe151652d7d804095be44b9
1Hanif1 <hanifmohammed869@gmail.com>
2021-12-26 16:42:08 +0530

    Band name generator

---
+++ Band name generator/main.py
@@ -0,0 +1,5 @@
+# Band name generator
+print("Confused what your band name should be? we got you XD")
+City = input("What city did you grow up in?: ")
+Pet = input("What is the name of your pet?: ")
+print(f"Your band name could be: {City} {Pet}")
