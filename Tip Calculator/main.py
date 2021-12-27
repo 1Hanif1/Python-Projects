c5624c9a29bd3985f244611a656a6f9b04e5c32d
1Hanif1 <hanifmohammed869@gmail.com>
2021-12-27 17:38:23 +0530

    Tip calculator

---
+++ Tip Calculator/main.py
@@ -0,0 +1,19 @@
+class TipCalculator():
+    def __init__(self):
+        print("------Welcome to the tip calculator------")
+        bill = float(input('What is the total bill? ₹'))
+        percentage = float(
+            input("What percentage of tip would you like to give? 10, 12 or 15?: "))
+        num_people = int(input('How many people to split the bill?: '))
+        tip = self.calcTip(bill, num_people, percentage)
+        print(f"Each person should pay ₹{tip}")
+
+    def calcTip(self, bill, num_people, percentage=0):
+        tip = bill * (percentage / 100)
+        total = bill + tip
+        split_per_person = round(total / num_people, 2)
+        return split_per_person
+
+
+if __name__ == '__main__':
+    TipCalculator()
