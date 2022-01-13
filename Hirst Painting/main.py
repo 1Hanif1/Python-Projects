a02b62a908dceb5386126437be119e575e04d89a
1Hanif1 <hanifmohammed869@gmail.com>
2022-01-13 16:15:08 +0530

    Hirst spot painting

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -1,27 +1,54 @@
-from turtle import Turtle, Screen
-import random
-turtle = Turtle()
-screen = Screen()
-screen.colormode(255)
-turtle.shape("circle")
-turtle.speed("fastest")
-turtle.pensize(15)
+from random import choice
+from turtle import Turtle, Screen, position, setpos
+import turtle
+
+# COLORGRAM MODULE TO EXTRACT COLORS FROM IMAGES
+# import colorgram
+
+# rgb = colorgram.extract('./image.jpg', 20)
+# colors = []
+
+# for obj in rgb:
+#     color = tuple([obj.rgb.r, obj.rgb.g, obj.rgb.b])
+#     colors.append(color)
+
+# print(colors)
 
 
-def draw_shape(angle):
-    turtle.fd(30)
-    turtle.rt(angle)
+class HirstPaint:
+    turtle = Turtle()
+    screen = Screen()
+    colors = [(236, 35, 108), (145, 28, 64), (239, 75, 35),
+              (6, 148, 93), (231, 168, 40), (184, 158, 46),
+              (44, 191, 233), (27, 127, 195), (126, 193, 74),
+              (253, 223, 0), (85, 28, 93), (173, 36, 97),
+              (246, 219, 44), (44, 172, 112), (215, 130, 165),
+              (215, 56, 27)]
+
+    def draw_spot(self, dot_size):
+        color = choice(self.colors)
+        self.turtle.dot(dot_size, color)
+
+    def draw_painting(self, dot_size, num_dots, space):
+        x = y = -((num_dots * space) / 2)
+        self.turtle.setpos(x, y)
+
+        for _ in range(num_dots):
+            for _ in range(num_dots):
+                self.draw_spot(dot_size)
+                self.turtle.fd(space)
+            y += space
+            self.turtle.setpos(x, y)
+
+    def __init__(self, dot_size, num_dots, space):
+        self.screen.colormode(255)
+        self.turtle.speed(0)
+        self.turtle.up()
+        self.draw_painting(dot_size, num_dots, space)
+        self.screen.exitonclick()
+        pass
 
 
-for _ in range(200):
-    red = random.randint(0, 255)
-    green = random.randint(0, 255)
-    blue = random.randint(0, 255)
-    turtle.color(red, green, blue)
-    angle = random.choice([0, 90, 180, 270])
-    draw_shape(angle)
-
-# 0+3, 1+3, 2+3, 3+3, 4+3, 5+3, 6+3. 7+3
-
-
-screen.exitonclick()
+if __name__ == "__main__":
+    # Arguments: size of dots, number of dots(n) ~ forms [n x n] matrix, space between dots.
+    HirstPaint(10, 20, 30)
