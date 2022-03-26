2ffb5487b31a2ec1d18b927c430a2826278cccf4
1Hanif1 <hanifmohammed869@gmail.com>
2022-03-26 16:36:16 +0530

    Form Submission

--- Blog/app.py
+++ Blog/app.py
@@ -1,7 +1,9 @@
-from pickle import GLOBAL
-from flask import Flask, render_template
+from os import environ
+from smtplib import SMTP
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
 from requests import get
-
+load_dotenv(dotenv_path="./cred.env")
 app = Flask(
     __name__,
     static_folder="./static",
@@ -9,6 +11,8 @@ app = Flask(
 )
 
 GLOBAL_DATA = []
+MY_EMAIL = environ["email"]
+PASSWORD = environ["password"]
 
 
 @app.route("/")
@@ -60,6 +64,51 @@ def get_post(id):
         subtitle=requested_post["subtitle"],
         imageurl="/static/assets/img/post-bg.jpg"
     )
+
+
+@app.route("/message", methods=["POST", "GET"])
+def send_message():
+    if request.method == "GET":
+        return render_template(
+            "contact.html",
+            title="Contact Me",
+            subtitle="You do the asking, I do the answering XD",
+            imageurl="/static/assets/img/contact-bg.jpg"
+        )
+
+    data = {
+        "name": request.form["user--name"],
+        "email": request.form["user--email"],
+        "number": request.form["user--number"],
+        "message": request.form["user--message"],
+    }
+    print(send_email(data))
+    name = request.form["user--name"]
+    return render_template(
+        "contact.html",
+        title="Message Sent",
+        subtitle=f"I'll get back to you soon, {name}",
+        imageurl="/static/assets/img/contact-bg.jpg"
+    )
+
+
+def send_email(data):
+    try:
+        name = data["name"]
+        email = data["email"]
+        number = data["number"]
+        message = data["message"]
+        message = f"{message}\nContact:{number}\nEmail:{email}"
+        with SMTP("smtp.gmail.com") as mail:
+            mail.starttls()
+            mail.login(user=MY_EMAIL, password=PASSWORD)
+            mail.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="hanifmohammed869@gmail.com",
+                msg=f"Subject:{name} wants to get in touch\n\n{message}"
+            )
+    except Exception:
+        pass
 
 
 if __name__ == '__main__':
